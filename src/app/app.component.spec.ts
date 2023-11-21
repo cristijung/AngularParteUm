d59e2d87d7b9f6edc34e4ecc56fb0edf1621276a
Cristina Jung <cristijung@gmail.com>
2023-11-21 15:08:26 -0300

    troca infernal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'algo' title`, () => {
+  it(`should have the 'AngularParteUm' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('algo');
+    expect(app.title).toEqual('AngularParteUm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, algo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularParteUm');
   });
 });
