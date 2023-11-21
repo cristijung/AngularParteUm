ab7ff6eb789ec5b537094c1329cc3e5ccd6b1183
Cristina Jung <cristijung@gmail.com>
2023-11-21 17:46:24 -0300

    rotas feitas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
