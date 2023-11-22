2e5a754622e9bd12ca778b74f177b3994b26f708
Cristina Jung <cristijung@gmail.com>
2023-11-22 10:25:43 -0300

    rotas finalizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {FirstComponent} from './first/first.component';
 import {SecondComponent} from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 
