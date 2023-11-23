4869f96e3436020a712fb1f2b9c04371177b773c
Cristina Jung <cristijung@gmail.com>
2023-11-23 15:02:41 -0300

    prints feitos

--- src/app/filho-dois/filho-dois.component.ts
+++ src/app/filho-dois/filho-dois.component.ts
@@ -6,12 +6,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './filho-dois.component.html',
-  styleUrl: './filho-dois.component.css'
+  styleUrl: './filho-dois.component.css',
 })
 export class FilhoDoisComponent {
   @Output() mensagemEnviada = new EventEmitter<string>();
 
   enviarMensagem() {
-    this.mensagemEnviada.emit('Olá, componente pai! Este é uma mensagem de Pai para Filho');
+    this.mensagemEnviada.emit(
+      'Olá, componente pai! Este é uma mensagem de Pai para Filho'
+    );
   }
 }
