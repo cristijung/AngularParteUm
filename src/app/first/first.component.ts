bec3ade04d43cc2333eb605d72f8fa1f701c7f2d
Cristina Jung <cristijung@gmail.com>
2023-11-24 10:21:56 -0300

    binding

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -9,6 +9,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './first.component.css'
 })
 export class FirstComponent {
+  title = 'Primeiro Componente';
+  stitle = 'Anatomia do Componente Angular';
+  legenda = 'A nova versão do Angular contempla várias mudanças significativas';
+  image = '../../assets/logo-angular.png'
+  alt = 'Nova marca do Angular';
 
 }
 
